67868dc403bab9eaa060a7dda26c634398e9d3bf
Sameer Ahmed <102585406+Mojojojono@users.noreply.github.com>
2025-02-06 01:40:36 +0500

    Adding functionality to the cart page and creating the orders page and place order feature also

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,5 +1,114 @@
-const CartPage = () => {
-  return <div>CartPage</div>;
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import Title from "../Components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import CartTotal from "../Components/CartTotal";
+import { toast } from "react-toastify";
+
+const Cart = () => {
+  const { products, currency, cartItems, updateQuantity, navigate } =
+    useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"Your"} text2={"Cart"} />
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  className="w-16 sm:w-20"
+                  src={productData.image[0]}
+                  alt=""
+                />
+                <div>
+                  <p className="text-sm sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="border bg-slate-50 px-4  sm:py-1">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                onChange={(e) =>
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value)
+                      )
+                }
+                className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-2"
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+              />
+              <img
+                onClick={() => updateQuantity(item._id, item.size, 0)}
+                className="w-4 mr-4 sm:w-5 cursor-pointer"
+                src={assets.bin_icon}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          {" "}
+          <CartTotal />
+          <div className="w-full text-end">
+            <button>
+              <button
+                className="bg-black text-white text-sm px-8 my-3 py-3 rounded hover:bg-blue-600"
+                onClick={() =>
+                  cartData.length === 0
+                    ? toast.error("Add items to cart first")
+                    : navigate("/place-order")
+                }
+              >
+                Proceed to Checkout
+              </button>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default CartPage;
+export default Cart;
--- frontend/src/Pages/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder.jsx
@@ -1,5 +1,98 @@
+import { useNavigate } from "react-router-dom";
+import CartTotal from "../Components/CartTotal";
+import Title from "../Components/Title";
+
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      <div className="flex- flex-col w-full gap-4 sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 mt-3 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 my-3 w-full"
+        />
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Province"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="number"
+            placeholder="Zipcode"
+            className="border border-gray-300 rounded py-1.5 px-3.5 my-3 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-300 rounded py-1.5 px-3.5 my-3 w-full"
+          />
+        </div>
+        <input
+          type="number"
+          placeholder="Phone No"
+          className="border border-gray-300 rounded py-1.5 px-3.5  w-full"
+        />
+      </div>
+
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          <div className=" flex items-center gap-3 border p-2 px-3 cursor-pointer">
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full bg-green-500`}
+            ></p>
+            <p className="text-gray-500 text-sm font-medium mx-4">
+              {" "}
+              Cash on Delivery
+            </p>
+          </div>
+        </div>
+        <div className="w-full text-center mt-5 ">
+          <button
+            onClick={() => navigate("/orders")}
+            className="bg-black text-white text-sm px-24 my-3 py-3 rounded hover:bg-blue-600"
+          >
+            Place Order
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
